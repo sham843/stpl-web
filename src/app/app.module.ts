83a3e2a17184855e0eaff7b521487e7cae5fb6ae
sham <sham.jadhav@shauryatechnosoft.com>
2022-04-06 09:48:28 +0530

    app module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from './core/core.module';
-
 
 @NgModule({
   declarations: [
@@ -18,7 +15,6 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
