8e2435a539d37a6dd811887dc8fc3fed2767b27a
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-04-14 16:53:52 +0530

    about us page component created

--- src/app/web/web-layout/web-layout-routing.module.ts
+++ src/app/web/web-layout/web-layout-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('../../web/home/home.module').then(m => m.HomeModule) },
+  { path: 'discover-us', loadChildren: () => import('../../web/about-us/about-us.module').then(m => m.AboutUsModule) },
 ];
 
 @NgModule({
