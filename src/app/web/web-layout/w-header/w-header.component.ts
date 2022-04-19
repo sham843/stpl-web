ce09e0e4d5b6c79489dcec124113b632748d1a53
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-04-19 18:16:49 +0530

    header redesigned

--- src/app/web/web-layout/w-header/w-header.component.ts
+++ src/app/web/web-layout/w-header/w-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener  } from '@angular/core';
 
 @Component({
   selector: 'app-w-header',
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./w-header.component.scss']
 })
 export class WHeaderComponent implements OnInit {
+  @HostListener('window:scroll', ['$event'])
 
+  onWindowScroll() {
+      let element = document.querySelector('.navbar') as HTMLElement;
+      if (window.pageYOffset > element.clientHeight) {
+        element.classList.add('navbar-scroll');
+      } else {
+        element.classList.remove('navbar-scroll');
+      }
+    }
   constructor() { }
 
   ngOnInit(): void {
