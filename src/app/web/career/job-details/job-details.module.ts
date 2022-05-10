820a1d24f8f195e7806e23230c9cd541839ae055
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-05-10 16:58:51 +0530

    apply now modal added on job details page

--- src/app/web/career/job-details/job-details.module.ts
+++ src/app/web/career/job-details/job-details.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { JobDetailsRoutingModule } from './job-details-routing.module';
 import { JobDetailsComponent } from './job-details.component';
-
+import { NgxFileDragDropModule } from 'ngx-file-drag-drop';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { JobDetailsComponent } from './job-details.component';
   ],
   imports: [
     CommonModule,
-    JobDetailsRoutingModule
+    JobDetailsRoutingModule,
+    NgxFileDragDropModule
   ]
 })
 export class JobDetailsModule { }
