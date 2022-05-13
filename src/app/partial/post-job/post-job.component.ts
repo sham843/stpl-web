8bcf766a5541978f316c9ce1038c1d612f072e6e
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-05-13 15:16:43 +0530

    add job modal popup added in post job page

--- src/app/partial/post-job/post-job.component.ts
+++ src/app/partial/post-job/post-job.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-job.component.css']
 })
 export class PostJobComponent implements OnInit {
-
+  public items: string[] = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
+  'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
+  'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin',
+  'Düsseldorf', 'Essen', 'Frankfurt'];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/partial/post-job/post-job.module.ts
+++ src/app/partial/post-job/post-job.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { PostJobRoutingModule } from './post-job-routing.module';
 import { PostJobComponent } from './post-job.component';
-
-
+import { NgxSelectModule } from 'ngx-select-ex';
+import { FormControl } from '@angular/forms';
 @NgModule({
   declarations: [
     PostJobComponent
   ],
   imports: [
     CommonModule,
-    PostJobRoutingModule
+    PostJobRoutingModule,
+    NgxSelectModule
   ]
 })
 export class PostJobModule { }
