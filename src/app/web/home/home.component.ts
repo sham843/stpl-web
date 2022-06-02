6eab83472bd8b9ad5117ec849c001ca096ec80be
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-06-02 17:09:29 +0530

    our products section designed on home page

--- src/app/web/home/home.component.ts
+++ src/app/web/home/home.component.ts
@@ -10,6 +10,13 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // var carouselA: any = document.getElementById('carouselExampleIndicators1')
+    // var carouselB: any = document.getElementById('carouselExampleIndicators')
+
+    // carouselA.addEventListener('slide.bs.carousel', function (e:any) {
+    //   var bsCarouselB = bootstrap.Carousel.getInstance(carouselB)
+    //   bsCarouselB.to(e.to)
+    // })
   }
 
 }
