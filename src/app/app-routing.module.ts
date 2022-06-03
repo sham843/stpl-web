78720f5294bc84760f772605a9fc4a02179e22cb
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-06-03 12:20:16 +0530

    solution details page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,6 @@ const routes: Routes = [
     component: PartialLayoutComponent,
     loadChildren: () => import('./partial/partial-layout/partial-layout.module').then(m => m.PartialLayoutModule)
   },
-  { path: 'project-details', loadChildren: () => import('./web/projects/project-details/project-details.module').then(m => m.ProjectDetailsModule) },
   
   { path: '**', component: PageNotFoundComponent},
 ];
