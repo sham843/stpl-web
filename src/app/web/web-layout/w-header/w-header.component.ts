9656a1af46471ca2dccf6c2454012d04b2f7ba66
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-06-06 16:22:41 +0530

    added auto modal poup for lead generation

--- src/app/web/web-layout/w-header/w-header.component.ts
+++ src/app/web/web-layout/w-header/w-header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, HostListener  } from '@angular/core';
 })
 export class WHeaderComponent implements OnInit {
   @HostListener('window:scroll', ['$event'])
-
+ 
   onWindowScroll() {
       let element = document.querySelector('.navbar') as HTMLElement;
       if (window.pageYOffset > element.clientHeight) {
